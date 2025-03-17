92f55f7fb6d9d4d9f8c6c6762638de37e90c5ac9
nazazulfiqi <zulfiqinaza@gmail.com>
2025-03-17 22:19:21 +0700

    feat(DetailProjectModule): Implement image carousel with NextImage in `src/modules/projects/detail/DetailProjectModule.tsx`

--- src/modules/projects/detail/DetailProjectModule.tsx
+++ src/modules/projects/detail/DetailProjectModule.tsx
@@ -19,29 +19,25 @@ const DetailProjectModule = () => {
       <div className="container">
         <Link
           href="/projects"
-          className="flex gap-2 hover:pl-3 transition-all duration-300 items-center mb-4 cursor-pointer w-[100px] "
+          className="flex gap-2 hover:pl-3 transition-all duration-300 items-center mb-4 cursor-pointer w-[100px]"
         >
           <IoArrowBackCircleOutline size={18} />
           <p className="text-lg">Back</p>
         </Link>
         <div className="border-b border-dashed">
-          <h1 className="text-3xl font-semibold mb-2">Projects</h1>
+          <h1 className="text-3xl font-semibold mb-2">Project Name</h1>
         </div>
 
         <div className="md:flex justify-between mt-2">
           <div className="flex items-center gap-1 mb-2">
-            <p className="font">Tech Stack : </p>
+            <p className="font">Tech Stack :</p>
             <div className="flex gap-2 items-center">
-              {/* {project.techstack.map((stack, index) => ( */}
-              {/* <Tooltip content={stack.name} key={index}> */}
               <Button
                 size={"icon"}
                 className="min-w-0 w-unit-5 h-unit-5 text-white bg-transparent rounded-none"
               >
                 <BiLogoTypescript className="text-blue-400" size={28} />
               </Button>
-              {/* </Tooltip> */}
-              {/* ))} */}
             </div>
           </div>
           <div className="flex items-center gap-4">
@@ -63,21 +59,41 @@ const DetailProjectModule = () => {
           </div>
         </div>
 
-        <div>
-          <Carousel>
-            <CarouselContent>
-              <CarouselItem>
+        <div className="my-8">
+          <Carousel className="w-full">
+            <CarouselContent className="flex">
+              <CarouselItem className="relative w-full h-[400px]">
                 <Image
-                  src="/images/project/website-kg.png"
-                  alt="image"
-                  width={500}
+                  src="/images/project/project-kg-cover.jpg"
+                  alt="Project-Image"
+                  width={800}
                   height={500}
+                  className="w-full h-full object-cover object-top rounded-lg"
+                  priority
+                />
+              </CarouselItem>
+              <CarouselItem className="relative w-full h-[400px]">
+                <Image
+                  src="/images/project/project-kg-cover.jpg"
+                  alt="Project-Image"
+                  width={800}
+                  height={500}
+                  className="w-full h-full object-cover object-top rounded-lg"
+                  priority
                 />
               </CarouselItem>
             </CarouselContent>
             <CarouselPrevious className="text-gray-900" />
             <CarouselNext className="text-gray-900" />
           </Carousel>
+        </div>
+
+        <div className="text-justify">
+          <p>
+            Developed the website using NextJS 13, React TypeScript, Nx
+            Monorepo, Tailwind CSS, Recoil, React Query, Zod Validation, React
+            Hook Form, and Headless UI on the front-end development.
+          </p>
         </div>
       </div>
     </div>
